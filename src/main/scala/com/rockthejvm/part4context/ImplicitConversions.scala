9662ada9d68c8da96bcc4c36cfa964bed7aa4630
Andrea Simone Costa <andrysimo1997@gmail.com>
2021-12-20 18:17:01 +0100

    complete implicit conversions

---
+++ src/main/scala/com/rockthejvm/part4context/ImplicitConversions.scala
@@ -0,0 +1,38 @@
+package com.rockthejvm.part4context
+
+// special import
+import scala.language.implicitConversions
+
+object ImplicitConversions {
+
+  // to convert a value of a type into of another type
+
+  case class Person(name: String) {
+    def greet(): String = s"Hi, I'm $name, how are you?"
+  }
+
+  val daniel = Person("Daniel")
+  val danielSaysHi = daniel.greet()
+
+  // special conversion instance
+  given string2Person: Conversion[String, Person] with
+    override def apply(x: String) = Person(x)
+
+  val danielSaysHi_v2 = "Daniel".greet() // Person("Daniel").greet(), automatically by the compiler
+
+  def processPerson(person: Person): String =
+    if (person.name.startsWith("J")) "OK"
+    else "NOT OK"
+
+  val isJaneOk = processPerson("Jane") // ok - compiler rewrites to processPerson(Person("Jane"))
+
+  /*
+    Use-cases for implicit conversions
+      - auto-box types
+      - use multiple (often unrelated) types with the same meaning in your code, interchangeably
+   */
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
